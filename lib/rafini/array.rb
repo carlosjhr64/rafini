b2c808b514dbd5b2316ebfadfac42c00d4bb247c
carlosjhr64 <carlosjhr64@gmail.com>
2014-11-08 17:06:30 -0800

    cleaner code

--- lib/rafini/array.rb
+++ lib/rafini/array.rb
@@ -2,15 +2,15 @@ module Rafini
   module Array
     refine ::Array do
       def joins(*p, &block)
-        s, j, l = '', p.length, length
-        if l > 0
-          s << self[0].to_s
-          1.upto(l-1) do |i|
-            s << ((i > j)? ((block)? block.call(i) : '' ) : p[i-1]).to_s
-            s << self[i].to_s
+        str = ''
+        if length > 1
+          str << self[0]
+          1.upto(length-1) do |i|
+            str << (p.empty? ? (block ? block.call(i).to_s : '') : p.shift.to_s)
+            str << self[i]
           end
         end
-        return s
+        return str
       end
 
       def per(b=nil)
