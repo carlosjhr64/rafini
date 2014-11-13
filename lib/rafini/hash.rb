aa8218bcfbfe577b5ddff79b683070fd4256f586
carlosjhr64 <carlosjhr64@gmail.com>
2014-11-13 08:50:29 -0800

    Added hash modify, supplement, and ammend.

--- lib/rafini/hash.rb
+++ lib/rafini/hash.rb
@@ -7,6 +7,52 @@ module Rafini
       def to_struct
         Struct.new(*self.keys).new(*self.values)
       end
+
+      # hash0.modify(hash1,...) #=> hash
+      #
+      # Updates hash with hashes.
+      # Overwrites existing elements and adds elements.
+      #    {a:'A',b:'B'}.modify({b:'X',c:'C'},{c:'Y',d:'D'}) #=> {a:'A',b:'X',c:'Y',d:'D'}
+      def modify(*hashes)
+        hashes.each do |hash|
+          hash.each do |key, value|
+            self[key] = value
+          end
+        end
+        self
+      end
+
+      # hash0.supplement(hash1,...) #=> hash
+      #
+      # Supplements hash with hashes.
+      # Adds missing elements only.
+      #   {a:'A',b:'B'}.supplement({b:'X',c:'C'},{c:'Y',d:'D'}) #=> {a:'A',b:'B',c:'C',d:'D'}
+      def supplement(*hashes)
+        hashes.each do |hash|
+          hash.each do |key, value|
+            self[key] = value unless self.has_key?(key)
+          end
+        end
+        self
+      end
+
+      # hash0.ammend(hash1,...)
+      #
+      # Ammends hash with hashes.
+      # Overwrites existing elements only.
+      #   {a:'A',b:'B'}.supplement({b:'X',c:'C'},{c:'Y',d:'D'}) #=> {a:'A',b:'X'}
+      def ammend(*hashes)
+        self.keys.each do |key|
+          hashes.each do |hash|
+            if hash.has_key?(key)
+              self[key] = hash[key]
+              break
+            end
+          end
+        end
+        self
+      end
+
     end
   end
 end
