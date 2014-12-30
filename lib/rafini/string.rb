efad0338b0eb5f5094f1cba9d1aa338665e0c54c
carlosjhr64 <carlosjhr64@gmail.com>
2014-12-30 06:51:16 -0800

    String#semantic

--- lib/rafini/string.rb
+++ lib/rafini/string.rb
@@ -5,7 +5,16 @@ module Rafini
       # 1) A camel kick, as in "I gotz camelized".
       # 2) "a_camel_kick" => "ACamelKick"
       def camelize(sep=/_/)
-        self.split(sep).map{ |word| word.capitalize }.join('')
+        self.split(sep).map{|word|word.capitalize }.join('')
+      end
+
+      # semantic:
+      # 'a.b.c'.semantic(1) #=> 'b'
+      # 'a.b.c'.semantic(0..1) #=> 'a.b'
+      # 'a.b.c'.semantic(0..2, '/') #=> 'b/c'
+      # 'a/b/c'.semantic(0..2, '.', /\//) #=> 'a.b.c'
+      def semantic(v,s='.',sx=/\./)
+        [*self.split(sx)[v]].join(s)
       end
     end
   end
