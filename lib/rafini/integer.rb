0c364d0565d4c48b22bff191eada1b20c52fc997
carlosjhr64 <carlosjhr64@gmail.com>
2021-01-11 09:17:05 -0800

    equivalent levels instead of multiples in odometer

--- lib/rafini/integer.rb
+++ lib/rafini/integer.rb
@@ -7,16 +7,24 @@ module Rafini
       #   30.odometer(2,3,5) #=> [0,0,0,1]
       #   ((60*60*24)*3 + (60*60)*12 + 60*15 + 30).odometer(60,60,24) #=> [30, 15, 12, 3]
       # Useful for making clocks, number scales, mayan long count... etc.
-      def odometer(*p)
+      def odometer(*levels, factors: true)
         raise RangeError, 'negative odometer' if self < 0
-        n, m, r  =  self, p.inject(1,:*), []
+        readings, remainder = [], self
 
-        p.reverse_each do |q|
-          r.unshift n/m
-          n, m  =  n%m, m/q
+        if factors
+          levels = levels.inject([1]){|m, f| m.push(m.last*f)}
+          levels.shift
         end
 
-        r.unshift n # remainder
+        levels.reverse_each do |level|
+          # in case of a float, floor
+          reading = (remainder/level).floor
+          readings.unshift reading
+          remainder = remainder%level
+        end
+
+        # in case of a float, round
+        readings.unshift remainder.round
       end
     end
   end
