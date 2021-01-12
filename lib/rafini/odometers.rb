6bb11db836c4f9ca6dc0d0440f2c156ebdec1b8e
carlosjhr64 <carlosjhr64@gmail.com>
2021-01-11 16:07:25 -0800

    odometers review

--- lib/rafini/odometers.rb
+++ lib/rafini/odometers.rb
@@ -3,79 +3,98 @@ require 'rafini/hash'
 
 module Rafini
   module Odometers
-    SEC2TIME = {
-      second:     60,
+    year = 365*24*60*60 + 6*60*60 + 9*60 + 9
+    SEC2TIME = { # levels
+      second:     1,
       minute:     60,
-      hour:       24,
-      day:        7,
-      week:       4,
-      month:      13,
-      year:       10,
-      decade:     10,
-      centurie:   10,
-      millennium: 10,
-      age:        10,
-      epoch:      10,
-      era:        5,
-      eon:        2,
-      gigaannum:  nil,
+      hour:       60*60,
+      day:        24*60*60,
+      week:       7*24*60*60,
+      month:      year/12.0,
+      year:       year,
+      decade:     10*year,
+      centurie:   100*year,
+      millennium: 1_000*year,
+      age:        10_000*year,
+      epoch:      100_000*year,
+      era:        1_000_000*year,
+      eon:        5_000_000*year,
+      gigaannum:  10_000_000*year,
     }
 
-    SCALE = {
+    SCALE = { # levels
       base: {
-        ones:         10,
-        tens:         10,
-        hundreds:     10,
-        thousands:    1_000,
+        one:         1,
+        ten:         10,
+        hundred:     100,
+        thousand:    10**3,
+        million:     10**6,
       },
       short: {
-        millions:     1_000,
-        billions:     1_000,
-        trillions:    1_000,
-        quadrillions: nil,
+        billion:     10**9,
+        trillion:    10**12,
+        quadrillion: 10**15,
       },
       long: {
-        millions:     1_000_000,
-        billions:     1_000_000,
-        trillions:    1_000_000,
-        quadrillions: nil,
+        billion:     10**12,
+        trillion:    10**18,
+        quadrillion: 10**24,
       },
     }
+
+    refine ::Hash do
+      def description
+        string = ''
+        reverse_each do |key, count|
+          s = (count==1)? '' : 's'
+          unless string.empty?
+            string << " and #{count} #{key}#{s}" if count > 0
+            break
+          end
+          next if count == 0
+          string << "#{count} #{key}#{s}"
+        end
+        return string
+      end
+    end
 
     refine ::Integer do
       using Rafini::Integer # Need Rafini::Integer for #odometer
       using Rafini::Hash    # Need Rafini::Hash for #to_struct
 
-      def odoread(scale)
-        counts = odometer(*scale.values[0..-2])
-        hash = ::Hash[scale.keys.zip(counts)]
-
-        string = ''
-        if self == 0
-          string << "0 #{scale.keys[0]}s"
-        else
-          hash.reverse_each do |key, count|
-            s = (count==1)? '' : 's'
-            unless string.empty?
-              string << " and #{count} #{key}#{s}" if count > 0
-              break
-            end
-            next if count == 0
-            string << "#{count} #{key}#{s}"
-          end
-        end
-        hash[:to_s] = string
-
-        hash.to_struct
+      def odoread(scale, **kw, &blk)
+        counts = odometer(*scale.values, **kw)
+        ::Hash[scale.keys.zip(counts)].to_struct(&blk)
       end
 
       # Integer#sec2time
       # Returns a struct with the different time scales for number of seconds.
-      # Note that the month(4 weeks)/year(13 months) are not meant to be exact.
       #   10_000.sec2time.to_s #=> "2 hours and 46 minutes"
       #   10_000.sec2time.hour #=> 2
       def sec2time
-        self.odoread(SEC2TIME)
+        odoread(SEC2TIME, factors:false) do
+          def to_s
+            string = nil
+            SEC2TIME.keys.reverse_each do |key|
+              count=self[key]
+              if string
+                if count > 0
+                  string << " and #{count} #{key}"
+                  string << 's' if count > 1
+                end
+                break
+              end
+              next if count==0
+              string = "#{count} #{key}"
+              string << 's' if count > 1
+            end
+            string = "0 #{SEC2TIME.first[0]}s" unless string
+            string
+          end
+          def to_i
+            SEC2TIME.to_a.map{|k,v|v*self[k]}.sum.round
+          end
+        end
       end
 
       # 1_230.illion.to_s #=> "1.23k"
@@ -94,36 +113,42 @@ module Rafini
       #   m.quadrillions #=> 888
       #   m.to_s #=> "888Q" It rounds up 888.7!
       def illion(type=:short)
-        keys   = SCALE[:base].keys   + SCALE[type].keys
-        values = SCALE[:base].values + SCALE[type].values
-        counts = self.odometer(*values[0..-2])
-
-        string = nil
-        if self < 1_000
-          string = self.to_s
-        elsif self < 1_000_000
-          d = (self<10_000)? 2 : (self<100_000)? 1 : 0
-          m = (self/1000.0).round(d)
-          string = "#{m}k"
-        else
-          (keys.length-1).downto(4) do |i|
-            next unless counts[i]>0
-            n = counts[i]
-            if n < 1_000
-              d = (n<10)? 2 : (n<100)? 1 : 0
-              n = (n + counts[i-1]/values[i-1].to_f).round(d)
-            else
-              n = n.illion
+        scale = SCALE[:base].merge SCALE[type]
+        struct = odoread(scale, factors:false) do
+          def scale=(scale)
+            @scale=scale
+          end
+          def type=(type)
+            @type=type
+          end
+          def to_s
+            number = to_i
+            return number.to_s if number < 1_000
+            if number < 1_000_000
+              precision = (number<10_000)? 2 : (number<100_000)? 1 : 0
+              return "#{(number/1000.0).round(precision)}K"
             end
-            string = "#{n}#{keys[i][0].upcase}"
-            break
+            keys = @scale.keys.reverse_each
+            loop do
+              key = keys.next
+              n = self[key]
+              next if n == 0
+              if n < 1_000
+                precision = (n<10)? 2 : (n<100)? 1 : 0
+                scale = @scale[key].to_f
+                f = (number/scale).round(precision)
+                return "#{f}#{key[0].upcase}"
+              end
+              return "#{n.illion}#{key[0].upcase}"
+            end
+          end
+          def to_i
+            @scale.to_a.map{|k,v|v*self[k]}.sum
           end
         end
-
-        hash = ::Hash[keys.zip(counts)]
-        hash[:to_s] = string
-
-        return hash.to_struct
+        struct.type  = type
+        struct.scale = scale
+        return struct
       end
     end
   end
