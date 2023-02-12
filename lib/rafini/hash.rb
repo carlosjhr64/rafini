ee94f44a80b0c64bf6e3a47a87e3aeed5fab6157
CarlosJHR64 <carlosjhr64@gmail.com>
2023-02-12 10:26:11 -0800

    rubocop hash.rb

--- lib/rafini/hash.rb
+++ lib/rafini/hash.rb
@@ -3,8 +3,8 @@ module Rafini
     refine ::Hash do
       # struct = hash.to_struct
       # Why not?
-      def to_struct(&blk)
-        Struct.new(*keys, &blk).new(*values)
+      def to_struct(&)
+        Struct.new(*keys, &).new(*values)
       end
 
       # hash0.supplement!(hash1,...) #=> hash
@@ -16,13 +16,13 @@ module Rafini
       def supplement!(*hashes)
         hashes.each do |hash|
           hash.each do |key, value|
-            self[key] = value unless self.has_key?(key)
+            self[key] = value unless key?(key)
           end
         end
         self
       end
       def supplement(...)
-        self.dup.supplement!(...)
+        dup.supplement!(...)
       end
 
       # hash0.amend(hash1,...)
@@ -32,13 +32,13 @@ module Rafini
       # only with first key value found in amending hashes.
       #   {a:'A',b:'B'}.amend({b:'X',c:'C'},{c:'Y',d:'D'}) #=> {a:'A',b:'X'}
       def amend!(*hashes)
-        self.each_key do |key|
-          value=hashes.find{_1.has_key? key}&.fetch(key) and self[key]=value
+        each_key do |key|
+          value=hashes.find{_1.key? key}&.fetch(key) and self[key]=value
         end
         self
       end
       def amend(...)
-        self.dup.amend!(...)
+        dup.amend!(...)
       end
     end
   end
