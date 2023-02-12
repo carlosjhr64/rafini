fe434a169f705c9573c94754a6184dab122e9a65
CarlosJHR64 <carlosjhr64@gmail.com>
2023-02-12 11:10:24 -0800

    rubocop string.rb

--- lib/rafini/string.rb
+++ lib/rafini/string.rb
@@ -5,7 +5,7 @@ module Rafini
       # 1) A camel kick, as in "I gotz camelized".
       # 2) "a_camel_kick" => "ACamelKick"
       def camelize(sep='_')
-        self.split(sep).map(&:capitalize).join
+        split(sep).map(&:capitalize).join
       end
 
       # semantic:
@@ -14,14 +14,14 @@ module Rafini
       # 'a.b.c'.semantic(0..2, join:'/') #=> 'b/c'
       # 'a/b/c'.semantic(0..2, split:'/', join:'.') #=> 'a.b.c'
       def semantic(v=(0..2), split:'.', join:'.')
-        [*self.split(split)[v]].join(join)
+        [*split(split)[v]].join(join)
       end
 
       # shellescape:
       # Same funtionality as Shellword's String#shellescape
       def shellescape
         # This is a contraction of Shellwords.escape function
-        self.gsub(/[^\w\-.,:+\/@\n]/,'\\\\\\&').gsub(/\n/,"'\n'")
+        gsub(/[^\w\-.,:+\/@\n]/,'\\\\\\&').gsub(/\n/,"'\n'")
       end
     end
   end
